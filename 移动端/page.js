9dba7d9cee7dd3ae6a7feeeb605ec1182003cc75
刘园215 <1803656569@qq.com>
2019-04-09 13:20:39 +0800

    首页

--- 移动端/page.js
+++ 移动端/page.js
@@ -7,3 +7,13 @@ $div1.on('tap',function(){
     $div2.eq($index).addClass('xian').siblings().removeClass('xian');
     // $div2.eq($index).show().siblings().hide();
 })
+
+var $div1=$('.head');
+// var $div2=$('#second');
+var $div2=$('#second .elect');
+$div1.on('click',function(){
+    var $index=$(this).index();
+    $div1.eq($index).addClass('show').siblings().removeClass('show');
+    $div2.eq($index).addClass('xian').siblings().removeClass('xian');
+    // $div2.eq($index).show().siblings().hide();
+})
