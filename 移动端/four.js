62aafd12ffa1dea54bb022be38cff62f3f6bc028
刘园215 <1803656569@qq.com>
2019-04-23 11:53:33 +0800

    首页

--- 移动端/four.js
+++ 移动端/four.js
@@ -1,43 +1,43 @@
 // 轮播图
-window.onload = function(){
-    var oBox = document.getElementById("box");
-    var imgs = oBox.getElementsByTagName("img");
-    var lits = oBox.getElementsByTagName("li");
-    var timer = null;
-    var cur = 0;
-    var len = lits.length;
+// window.onload = function(){
+//     var oBox = document.getElementById("box");
+//     var imgs = oBox.getElementsByTagName("img");
+//     var lits = oBox.getElementsByTagName("li");
+//     var timer = null;
+//     var cur = 0;
+//     var len = lits.length;
 
-    timer = setInterval( autoPlay,1500 );
+//     timer = setInterval( autoPlay,1500 );
 
-    oBox.onmouseover = function(){
-        clearInterval( timer );
-    }
-    oBox.onmouseout = function(){
-        timer = setInterval( autoPlay,1500 );
-    }
-    for( var i =0; i<len; i++ ){
-        (function(j){
-            lits[j].onclick = function(){
-                changePic(j);
-                cur = j;
-            }
-        })(i);
-    }
+//     oBox.onmouseover = function(){
+//         clearInterval( timer );
+//     }
+//     oBox.onmouseout = function(){
+//         timer = setInterval( autoPlay,1500 );
+//     }
+//     for( var i =0; i<len; i++ ){
+//         (function(j){
+//             lits[j].onclick = function(){
+//                 changePic(j);
+//                 cur = j;
+//             }
+//         })(i);
+//     }
 
-    function autoPlay(){
-        cur++;
-        if( cur>=len ){ cur = 0 }
-        changePic( cur );
-    }
-    function changePic( curIndex ){
-        for( var i= 0;i<len;i++ ){
-            imgs[i].className = "unshow";
-            lits[i].className = "";
-        }
-        imgs[curIndex].className = "show";
-        lits[curIndex].className = "active";
-    }
-}
+//     function autoPlay(){
+//         cur++;
+//         if( cur>=len ){ cur = 0 }
+//         changePic( cur );
+//     }
+//     function changePic( curIndex ){
+//         for( var i= 0;i<len;i++ ){
+//             imgs[i].className = "unshow";
+//             lits[i].className = "";
+//         }
+//         imgs[curIndex].className = "show";
+//         lits[curIndex].className = "active";
+//     }
+// }
 
 // 导航
 var oWrapper = document.getElementById('wrapper');
@@ -56,18 +56,21 @@ var oBack = document.getElementById('back');
 oNavbs.onclick = function(){
     oNavc.style.display='block';
     oWrapper.style.position='absolute';
-    oWrapper.style.left='280px';
+    oWrapper.style.left='270px';
 };
 oRe.onclick = function(){
     oNavc.style.display='none';
     oWrapper.style.position='absolute';
     oWrapper.style.left='0';
 }
+oBack.onclick = function(){
+    $('html,body').scrollTop(0);
+}
 // 移动
 oNavbs.tag = function(){
     oNavc.style.display='block';
     oWrapper.style.position='absolute';
-    oWrapper.style.left='280px';
+    oWrapper.style.left='270px';
 };
 oRe.tag = function(){
     oNavc.style.display='none';
@@ -85,11 +88,17 @@ oNavc2.onclick = function(){
     oNavc.style.display='none';
     oWrapper.style.position='absolute';
     oWrapper.style.left='0';
-    // oWrapper.style.top='-7rem';
+    $('html,body').scrollTop(700);
 };
 // 移动
 oNavc1.tag = function(){
     oNavc.style.display='none';
     oWrapper.style.position='absolute';
     oWrapper.style.left='0';
+};
+oNavc2.tag = function(){
+    oNavc.style.display='none';
+    oWrapper.style.position='absolute';
+    oWrapper.style.left='0';
+    $('html,body').scrollTop(200);
 };
