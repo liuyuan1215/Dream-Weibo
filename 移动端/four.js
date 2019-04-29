e78eca7fc40e47e8e4613478e662976c18029b09
刘园215 <1803656569@qq.com>
2019-04-29 13:00:13 +0800

    首页

--- 移动端/four.js
+++ 移动端/four.js
@@ -1,71 +1,43 @@
 // 轮播图
-// window.onload = function(){
-//     var oBox = document.getElementById("box");
-//     var imgs = oBox.getElementsByTagName("img");
-//     var lits = oBox.getElementsByTagName("li");
-//     var timer = null;
-//     var cur = 0;
-//     var len = lits.length;
+window.onload = function(){
+    var oBox = document.getElementById("box");
+    var imgs = oBox.getElementsByTagName("img");
+    var lits = oBox.getElementsByTagName("li");
+    var timer = null;
+    var cur = 0;
+    var len = lits.length;
 
-//     timer = setInterval( autoPlay,1500 );
+    timer = setInterval( autoPlay,1500 );
 
-//     oBox.onmouseover = function(){
-//         clearInterval( timer );
-//     }
-//     oBox.onmouseout = function(){
-//         timer = setInterval( autoPlay,1500 );
-//     }
-//     for( var i =0; i<len; i++ ){
-//         (function(j){
-//             lits[j].onclick = function(){
-//                 changePic(j);
-//                 cur = j;
-//             }
-//         })(i);
-//     }
+    oBox.onmouseover = function(){
+        clearInterval( timer );
+    }
+    oBox.onmouseout = function(){
+        timer = setInterval( autoPlay,1500 );
+    }
+    for( var i =0; i<len; i++ ){
+        (function(j){
+            lits[j].onclick = function(){
+                changePic(j);
+                cur = j;
+            }
+        })(i);
+    }
 
-//     function autoPlay(){
-//         cur++;
-//         if( cur>=len ){ cur = 0 }
-//         changePic( cur );
-//     }
-//     function changePic( curIndex ){
-//         for( var i= 0;i<len;i++ ){
-//             imgs[i].className = "unshow";
-//             lits[i].className = "";
-//         }
-//         imgs[curIndex].className = "show";
-//         lits[curIndex].className = "active";
-//     }
-// }
-
-// 时间
-// var time1 = document.getElementById('time1');
-// var now = new Date(),s;
-//     var day;
-//     if(now.getDay()==1){
-//         day='一';
-//     }else if(now.getDay()==2){
-//         day='二';
-//     }
-//     else if(now.getDay()==3){
-//         day='三';
-//     }
-//     else if(now.getDay()==4){
-//         day='四';
-//     }
-//     else if(now.getDay()==5){
-//         day='五';
-//     }
-//     else if(now.getDay()==6){
-//         day='六';
-//     }else{
-//         day='日';
-//     }
-//     s=now.getFullYear()+"年"+(now.getMonth()+1)+"月"+now.getDate()+"日"+"<br>"+"星期"+day+"<br>"+now.getHours()+"时"+now.getMinutes()+"分"+now.getSeconds()+"秒";
-//     var oDiv = document.createElement('div');
-//     oDiv.innerHTML = s;
-//     time1.appendChild(oDiv);
+    function autoPlay(){
+        cur++;
+        if( cur>=len ){ cur = 0 }
+        changePic( cur );
+    }
+    function changePic( curIndex ){
+        for( var i= 0;i<len;i++ ){
+            imgs[i].className = "unshow";
+            lits[i].className = "";
+        }
+        imgs[curIndex].className = "show";
+        lits[curIndex].className = "active";
+    }
+}
 
 // 导航
 var oWrapper = document.getElementById('wrapper');
