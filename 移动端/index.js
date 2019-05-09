9bb7e167918c330aab19967697a32dba94c1a919
刘园215 <1803656569@qq.com>
2019-05-09 19:13:00 +0800

    首页

--- 移动端/index.js
+++ 移动端/index.js
@@ -1,43 +1,43 @@
 // 轮播图
-// window.onload = function () {
-//     var oBox = document.getElementById("box");
-//     var imgs = oBox.getElementsByTagName("img");
-//     var lits = oBox.getElementsByTagName("li");
-//     var timer = null;
-//     var cur = 0;
-//     var len = lits.length;
+window.onload = function () {
+    var oBox = document.getElementById("box");
+    var imgs = oBox.getElementsByTagName("img");
+    var lits = oBox.getElementsByTagName("li");
+    var timer = null;
+    var cur = 0;
+    var len = lits.length;
 
-//     timer = setInterval(autoPlay, 1500);
+    timer = setInterval(autoPlay, 1500);
 
-//     oBox.onmouseover = function () {
-//         clearInterval(timer);
-//     }
-//     oBox.onmouseout = function () {
-//         timer = setInterval(autoPlay, 1500);
-//     }
-//     for (var i = 0; i < len; i++) {
-//         (function (j) {
-//             lits[j].onclick = function () {
-//                 changePic(j);
-//                 cur = j;
-//             }
-//         })(i);
-//     }
+    oBox.onmouseover = function () {
+        clearInterval(timer);
+    }
+    oBox.onmouseout = function () {
+        timer = setInterval(autoPlay, 1500);
+    }
+    for (var i = 0; i < len; i++) {
+        (function (j) {
+            lits[j].onclick = function () {
+                changePic(j);
+                cur = j;
+            }
+        })(i);
+    }
 
-//     function autoPlay() {
-//         cur++;
-//         if (cur >= len) { cur = 0 }
-//         changePic(cur);
-//     }
-//     function changePic(curIndex) {
-//         for (var i = 0; i < len; i++) {
-//             imgs[i].className = "unshow";
-//             lits[i].className = "";
-//         }
-//         imgs[curIndex].className = "show";
-//         lits[curIndex].className = "active";
-//     }
-// }
+    function autoPlay() {
+        cur++;
+        if (cur >= len) { cur = 0 }
+        changePic(cur);
+    }
+    function changePic(curIndex) {
+        for (var i = 0; i < len; i++) {
+            imgs[i].className = "unshow";
+            lits[i].className = "";
+        }
+        imgs[curIndex].className = "show";
+        lits[curIndex].className = "active";
+    }
+}
 
 // 导航
 var oWrapper = document.getElementById('wrapper');
