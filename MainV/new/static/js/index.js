8cfc462eb37109ee1dbb5df093a7e9db7cb52ddf
刘园215 <1803656569@qq.com>
2019-05-28 22:54:07 +0800

    首页

--- MainV/new/static/js/index.js
+++ MainV/new/static/js/index.js
@@ -142,50 +142,24 @@ oNavc4.tag = function () {
 };
 
 // 钟表
-// var oDial = document.getElementById("dial");
-// var oHour = document.getElementById("hour");
-// var oMinute = document.getElementById("minute");
-// var oSecond = document.getElementById("second");
-// var lis = "";
-// for (var i = 0; i < 60; i++) {
-//     /*var oLi = document.createElement("li");
-//     oDial.appendChild(oLi);*/
-//     lis += "<li style='transform: rotate(" + i * 6 + "deg);'></li>";
-// }
-// oDial.innerHTML = lis;
-
-// run();
-
-// setInterval(run, 1000);
-
-// function run() {
-//     var now = new Date();
-//     var second = now.getSeconds();
-//     var minute = now.getMinutes() + second / 60;
-//     var hour = now.getHours();//+ minute / 60
-//     oHour.style.transform = "rotate(" + (hour * 30 + minute / 60 * 30) + "deg)";
-//     oMinute.style.transform = "rotate(" + minute * 6 + "deg)";
-//     oSecond.style.transform = "rotate(" + second * 6 + "deg)";
-// }
-
-// var box3 = document.getElementsByClassName("box3")[0];
-// var oDiv = document.createElement('div');
-// var myClock = function () {
-//     var Day = new Date();
-//     var year = Day.getFullYear();
-//     var month = Day.getMonth();
-//     var data = Day.getDate();
-//     var day = Day.getDay();
-//     var hour = Day.getHours();
-//     var minu = Day.getMinutes();
-//     var second = Day.getSeconds();
-//     month = month + 1;
-//     var str = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
-//     var week = str[day];
-//     var time = year + "年" + month + "月" + data + "日" + " " + week + " " + hour + " ：" + minu + "：" + second;
-//     oDiv.innerHTML = time;
-// }
-// setInterval(function () {
-//     myClock();
-// }, 1000);
-// box3.appendChild(oDiv);
+var box3 = document.getElementsByClassName("box3")[0];
+var oDiv = document.createElement('div');
+var myClock = function () {
+    var Day = new Date();
+    var year = Day.getFullYear();
+    var month = Day.getMonth();
+    var data = Day.getDate();
+    var day = Day.getDay();
+    var hour = Day.getHours();
+    var minu = Day.getMinutes();
+    var second = Day.getSeconds();
+    month = month + 1;
+    var str = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    var week = str[day];
+    var time = year + "年" + month + "月" + data + "日" + " " + week + " " + hour + " ：" + minu + "：" + second;
+    oDiv.innerHTML = time;
+}
+setInterval(function () {
+    myClock();
+}, 1000);
+box3.appendChild(oDiv);
